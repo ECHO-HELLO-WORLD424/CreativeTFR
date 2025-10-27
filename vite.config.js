29b83f2bb391ce531e4a372f5f0875393a7ae4a8
Patrick_Chen <patrickechohelloworld@outlook.com>
2025-10-27 22:01:58 +0800

    For my self-hosted instance

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,17 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), Components({
-    resolvers: [
-      PrimeVueResolver()
-    ]
-  })],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [vue(), vueDevTools(), Components({
+        resolvers: [
+            PrimeVueResolver()
+        ]
+    })],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-  },
-});
+    server: {
+        allowedHosts: ["tfr.errisntnil.icu"]
+    }
+})
